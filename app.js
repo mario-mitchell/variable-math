344acd2770569dbad72c3873c42bb107584454c0
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2022-05-20 11:01:44 -0700

    Math assignment.

---
+++ app.js
@@ -0,0 +1,50 @@
+/* Future
+
+let age = 30;
+let future = 5 + age;
+console.log("In five years I will be " + future)
+
+*/
+
+/* Adder
+
+let num1 = 5;
+let num2 = 9;
+let sum = num1 + num2;
+
+console.log(num1 + " Plus " + num2 + " Equal " + sum);
+
+*/
+
+/* Greeter
+
+let name = "Mario Mitchell"
+
+console.log("Hello my name is " + name + ".");
+
+*/
+
+/* Remainder
+
+let num3 = 7;
+let num4 = 4;
+let sum2 = num3 / num4;
+
+
+console.log(num3 + " divided by " + num4 + " = " + sum2 + "." );
+*/
+
+let x1 = 5;
+let x2 = 8;
+let y1 =9;
+let y2 = 4;
+
+let sum1 = x1 - y1;
+let sum2 = x2 - y2;
+let sqSum1 = sum1 * sum1;
+let sqSum2 = sum2 * sum2;
+let addSqSum = sqSum1 + sqSum2;
+let distance = Math.sqrt(addSqSum)
+
+console.log("The distance between the two points is " + distance);
+
