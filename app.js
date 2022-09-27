347af03d6586dc7d009d88e609d10f68614c49a8
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2022-09-27 07:42:19 -0700

    Used remainder in assignment

--- app.js
+++ app.js
@@ -24,27 +24,27 @@ console.log("Hello my name is " + name + ".");
 
 */
 
-/* Remainder
+//Remainder
 
 let num3 = 7;
 let num4 = 4;
-let sum2 = num3 / num4;
+let sum2 = num3 % num4;
 
 
 console.log(num3 + " divided by " + num4 + " = " + sum2 + "." );
-*/
 
-let x1 = 5;
-let x2 = 8;
-let y1 =9;
-let y2 = 4;
 
-let sum1 = x1 - y1;
-let sum2 = x2 - y2;
-let sqSum1 = sum1 * sum1;
-let sqSum2 = sum2 * sum2;
-let addSqSum = sqSum1 + sqSum2;
-let distance = Math.sqrt(addSqSum)
+// let x1 = 5;
+// let x2 = 8;
+// let y1 =9;
+// let y2 = 4;
 
-console.log("The distance between the two points is " + distance);
+// let sum1 = x1 - y1;
+// let sum2 = x2 - y2;
+// let sqSum1 = sum1 * sum1;
+// let sqSum2 = sum2 * sum2;
+// let addSqSum = sqSum1 + sqSum2;
+// let distance = Math.sqrt(addSqSum)
+
+// console.log("The distance between the two points is " + distance);
 
